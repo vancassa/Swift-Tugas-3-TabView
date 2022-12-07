be775e89b73477ff8f56568e251f1bc0ad4f619b
Vanessa Cassandra <vanessa.cassandra.s@gmail.com>
2022-12-07 11:27:13 +0700

    Initial Commit

---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Vanessa Cassandra on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
